797dd9f79fff5b620770e50a54ff8add46d2ad10
berg8201 <52533671+berg8201@users.noreply.github.com>
2021-11-07 21:31:34 +0100

    Add files via upload

---
+++ src/main/java/com/kaanbe/springboot/Controller.java
@@ -0,0 +1,17 @@
+package com.kaanbe.springboot;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class Controller {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("It just works");
+    }
+    
+}
